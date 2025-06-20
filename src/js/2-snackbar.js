10a938319fa16ce2a1a6aaf754d5e07a9d3e0071
Dmytro <magnit2006@gmail.com>
2025-06-21 00:32:00 +0300

    first commit

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,39 +1,62 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const formEl = document.querySelector('.form');
+const formData = document.querySelector('.form');
+const delay = document.querySelector('input[name="delay"]');
 
-formEl.addEventListener('submit', formSubmitHandler);
-
-function formSubmitHandler(e) {
-  e.preventDefault();
-  const delay = +e.currentTarget.elements.delay.value;
-  const state = e.currentTarget.elements.state.value;
-
-  createNewPromise(delay, state);
-}
-
-function createNewPromise(delay, state) {
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (state === 'fulfilled') {
-        resolve(delay);
-      }
-      reject(delay);
-    }, delay);
-  })
-    .then(value => {
-      iziToast.show({
-        color: 'green',
-        position: 'topRight',
-        message: `✅ Fulfilled promise in ${value}ms`,
-      });
-    })
-    .catch(value => {
-      iziToast.show({
-        color: 'red',
-        position: 'topRight',
-        message: `❌ Rejected promise in ${value}ms`,
-      });
+function valid() {
+  const delayValue = Number(delay.value);
+  if (isNaN(delayValue) || delayValue <= 0) {
+    iziToast.warning({
+      title: 'Warning',
+      message: 'Введіть ціле число',
+      position: 'topRight',
+      messageColor: 'black',
+      color: 'lightblue',
+      titleColor: 'black',
     });
+    return false;
+  } else {
+    console.log('Введено число:', delayValue);
+    return true;
+  }
 }
+
+formData.addEventListener('submit', ev => {
+  ev.preventDefault();
+  const inputStates = document.querySelector('input[name=state]:checked');
+  if (!valid()) return;
+
+  const delayValue = Number(delay.value);
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (inputStates.value === 'fulfilled') {
+        resolve(delayValue);
+        console.log(`create with ${delayValue}`);
+      } else if (inputStates.value === 'rejected') {
+        reject(delayValue);
+        console.log(`NOT create ${delayValue}`);
+      }
+    }, Number(delay.value));
+  });
+  promise
+    .then(res =>
+      iziToast.success({
+        title: 'OK',
+        message: `✅ Fulfilled promise in ${delayValue}ms`,
+        position: 'topRight',
+        messageColor: '#fff',
+        color: '#59A10D',
+        titleColor: '#fff',
+      })
+    )
+    .catch(error =>
+      iziToast.error({
+        title: 'ERROR',
+        message: `❌ Rejected promise in ${delayValue}ms`,
+        position: 'topRight',
+        messageColor: '#fff',
+        titleColor: '#fff',
+      })
+    );
+});
